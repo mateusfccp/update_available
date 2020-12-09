18a245a4b59c1c0d91230772961b14c8f4e479f2
Mateus Felipe <mateusfccp@gmail.com>
2020-12-09 14:02:40 -0300

    Provide a little more documentation on the interface

--- update_available_platform_interface/lib/src/interface.dart
+++ update_available_platform_interface/lib/src/interface.dart
@@ -2,6 +2,7 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import 'availability.dart';
 
+/// The plarform interface for update_available.
 abstract class UpdateAvailablePlatform extends PlatformInterface {
   UpdateAvailablePlatform() : super(token: _token);
 
@@ -9,10 +10,16 @@ abstract class UpdateAvailablePlatform extends PlatformInterface {
 
   static UpdateAvailablePlatform _instance = UnimplementedUpdateAvailable();
 
+  /// The default instance of [UpdateAvailablePlatform] to use.
+  ///
+  /// Platform-specific plugins should override this with their own class
+  /// that extends [UpdateAvailablePlatform] when they register themselves.
+  ///
+  /// Defaults to [MethodChannelLocation].
   static UpdateAvailablePlatform get instance => _instance;
 
   /// Platform-specific plugins should set this with their own platform-specific
-  /// class that extends [UrlLauncherPlatform] when they register themselves.
+  /// class that extends [UpdateAvailablePlatform] when they register themselves.
   static set instance(UpdateAvailablePlatform instance) {
     PlatformInterface.verifyToken(instance, _token);
     _instance = instance;
@@ -23,4 +30,5 @@ abstract class UpdateAvailablePlatform extends PlatformInterface {
       );
 }
 
+/// The unimplemented platform for [UpdateAvailablePlatform].
 class UnimplementedUpdateAvailable extends UpdateAvailablePlatform {}
